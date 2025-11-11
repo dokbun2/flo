b03c8a3257d8c271b58ec57d86d953b95db4af40
haruoff <ggamsire@gmail.com>
2025-11-11 09:41:46 +0900

    수ㅇ하기 오류
    11

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
+    unoptimized: true, // Unsplash 403 오류 방지를 위해 이미지 최적화 비활성화
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
